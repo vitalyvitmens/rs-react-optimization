bddd0ac3f3ffcbbfbe4fd88fe5885af02529a0d8
vitalyvitmens <otm_@tut.by>
2024-02-17 01:13:27 +0300

    chore(src\pages\Category\Category.jsx): добавляет логику решения задачи

--- src/pages/Category/Category.jsx
+++ src/pages/Category/Category.jsx
@@ -1,31 +1,45 @@
-import { useEffect, useState } from 'react'
-import { Link, useSearchParams, useParams } from 'react-router-dom'
+import { useCallback, useRef, useState } from 'react'
+import { Link, useSearchParams } from 'react-router-dom'
 import { CustomSelect } from '../../components'
 import { NotFound } from '../../pages'
+import { useFetchCategory } from '../../hooks'
 import styles from './Category.module.css'
 
 export const Category = () => {
 	const [searchParams, setSearchParams] = useSearchParams()
-	const { category } = useParams()
 	const sort = searchParams.get('sort')
-	const [data, setData] = useState([])
-	const [loading, setLoading] = useState(true)
+	const [query, setQuery] = useState('')
+	const [pageNumber, setPageNumber] = useState(3)
 
-	useEffect(() => {
-		async function fetchData() {
-			try {
-				const json = await import('../../db.json')
-				const array = json[category]
-				setData(array)
-				setLoading(false)
-			} catch (error) {
-				console.error(error)
-				setLoading(false)
+	const { loading, error, categories, hasMore, category, id } = useFetchCategory(
+		query,
+		pageNumber
+	)
+
+	const observer = useRef()
+	const lastNodeRef = useCallback(
+		(node) => {
+			if (loading) return
+
+			if (observer.current) {
+				observer.current.disconnect()
 			}
-		}
 
-		fetchData()
-	}, [category])
+			observer.current = new IntersectionObserver((entries) => {
+				if (entries[0].isIntersecting && hasMore) {
+					setPageNumber((prevState) => prevState + 1)
+					console.log('####: VISIBLE')
+				}
+			})
+
+			if (node) {
+				observer.current.observe(node)
+			}
+
+			console.log('####: node', node)
+		},
+		[hasMore, loading]
+	)
 
 	const sortByCreated = (array, order) => {
 		const copy = [...(array || [])]
@@ -45,6 +59,8 @@ export const Category = () => {
 
 	const handleChangeSort = (event) => {
 		const value = event.target.value
+		setQuery(id)
+		setPageNumber(1)
 		setSearchParams({ sort: value })
 	}
 
@@ -64,17 +80,27 @@ export const Category = () => {
 					onChange={handleChangeSort}
 				/>
 			</form>
-			{!data || loading ? (
-				<span>Загрузка...</span>
-			) : (
-				<ul>
-					{sortByCreated(data, sort).map((item) => (
-						<li key={item.id}>
-							<Link to={`/${category}/${item.id}`}>{item.name}</Link>
-						</li>
-					))}
-				</ul>
-			)}
+			<ul>
+				{sortByCreated(categories, sort).map((item, index) => {
+					if (categories.length - 20 === index + 1) {
+						return (
+							<li ref={lastNodeRef} key={item.id}>
+								<Link to={`/${category}/${item.id}`}>{item.name}</Link>
+							</li>
+						)
+					} else {
+						return (
+							<li key={item.id}>
+								<Link to={`/${category}/${item.id}`}>{item.name}</Link>
+							</li>
+						)
+					}
+				})}
+				{loading && (
+					<div style={{ color: 'red', fontSize: '3rem' }}>Loading...</div>
+				)}
+				{error && <div>Error</div>}
+			</ul>
 		</div>
 	)
 }
