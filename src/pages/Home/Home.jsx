bd2bbeed1d65dc620ed253ac59a4349e15cb2ffb
vitalyvitmens <otm_@tut.by>
2024-02-16 15:01:39 +0300

    fix(src\pages\Home\Home.jsx): удаляет отображение Navigation

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,8 @@
-import { Navigation } from '../../components'
 import styles from './Home.module.css'
 
 export const Home = () => {
 	return (
 		<div className={styles.Home}>
-			<Navigation />
 			<h1>Добро пожаловать в приложение по вселенной Рика и Морти!</h1>
 		</div>
 	)
