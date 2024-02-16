1a757e4c085e977bf90c23fbe46fd7c3c68a8e6f
vitalyvitmens <otm_@tut.by>
2024-02-16 14:59:46 +0300

    chore(src\components\index.js): удаляет экспорт Navigation

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
 export * from './Button/Button.jsx'
 export * from './CustomSelect/CustomSelect.jsx'
-export * from './Navigation/Navigation.jsx'
 export * from './PrivateRoute/PrivateRoute'
 export * from './AuthStatus/AuthStatus.jsx'
 export * from './CustomInput/CustomInput.jsx'
